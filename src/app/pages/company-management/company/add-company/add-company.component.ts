7998e881cc441893114ed5449d2b088be8d75607
Mainak <ph-mainak@mobiloitte.com>
2021-01-12 13:08:00 +0530

    view company list api integrated with add comapny validations

--- src/app/pages/company-management/company/add-company/add-company.component.ts
+++ src/app/pages/company-management/company/add-company/add-company.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl , Validators, FormArray, FormBuilder} from '@angular/forms';
 import { Router } from '@angular/router';
 import { MainService } from 'src/app/provider/main.service';
 
@@ -20,12 +20,12 @@ export class AddCompanyComponent implements OnInit {
   // add form validation
   addFormValidation() {
     this.addForm = new FormGroup({
-      'firstName': new FormControl(''),
-      'lastName': new FormControl(''),
-      'mobileNo': new FormControl(''),
-      'emainId': new FormControl(''),
-      'companyName': new FormControl(''),
-      'companyAddress': new FormControl(''),
+      'firstName': new FormControl('',[Validators.required, Validators.pattern(/^[a-zA-Z ]*$/i)]),
+      'lastName': new FormControl('',[Validators.required, Validators.pattern(/^[a-zA-Z ]*$/i)]),
+      'phoneNo': new FormControl('',[Validators.required, Validators.pattern(/^[1-9][0-9]{9,13}$/)]),
+      'email': new FormControl('',[Validators.required, Validators.pattern(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,9}|[0-9]{1,3})(\]?)$/i)]),
+      'companyName': new FormControl('',[Validators.required]),
+      'companyAddress': new FormControl('',[Validators.required]),
       'city': new FormControl(''),
       'state': new FormControl(''),
       'aadhaarNo': new FormControl(''),
