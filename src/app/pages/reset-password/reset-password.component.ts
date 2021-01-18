430b1216a5b053299c8f046fa32ced6fcc4ecd7b
Arvind Sharma <ph-arvindsharma@mobiloitte.com>
2021-01-18 22:57:47 +0530

    reset password user-management case add

--- src/app/pages/reset-password/reset-password.component.ts
+++ src/app/pages/reset-password/reset-password.component.ts
@@ -13,7 +13,7 @@ export class ResetPasswordComponent implements OnInit {
   resetPasswordForm: FormGroup
   clientId: any;
   resetParamData: any;
-  roleArray: any = ['COMPANY', 'FLEET', 'SUPPLIER', 'DRIVER','USER'];
+  roleArray: any = ['COMPANY', 'FLEET', 'SUPPLIER', 'DRIVER', 'USER'];
 
   constructor(
     private router: Router, public service: MainService, private activatedRoute: ActivatedRoute) {
@@ -71,7 +71,7 @@ export class ResetPasswordComponent implements OnInit {
           case 'DRIVER':
             this.router.navigate(['/list-of-driver'])
             break;
-            case 'USER':
+          case 'USER':
             this.router.navigate(['/list-of-company-user'])
             break;
           default:
@@ -86,7 +86,7 @@ export class ResetPasswordComponent implements OnInit {
     })
   }
 
-  back(){
+  back() {
     switch (this.resetParamData.role) {
       case 'COMPANY':
         this.router.navigate(['/list-of-companies'])
@@ -99,6 +99,9 @@ export class ResetPasswordComponent implements OnInit {
         break;
       case 'DRIVER':
         this.router.navigate(['/list-of-driver'])
+        break;
+      case 'USER':
+        this.router.navigate(['/list-of-company-user'])
         break;
       default:
         this.router.navigate(['/dashboard'])
